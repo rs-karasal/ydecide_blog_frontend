d7a1612266cf41a00f282a4958168d200c3d82b4
millennial <ereskarasal94@gmail.com>
2024-10-06 20:34:15 +0500

    createdauth page

---
+++ src/pages/LoginPage.jsx
@@ -0,0 +1,72 @@
+import { Form, Input, Button, message } from "antd";
+import axiosInstance from "../axiosInstance";
+import { useState } from "react";
+
+const LoginPage = () => {
+  const [loading, setLoading] = useState(false);
+
+  const onFinish = async (values) => {
+    setLoading(true);
+    try {
+      const response = await axiosInstance.post("/login", {
+        username: values.username,
+        Password: values.password,
+      });
+
+      if (response.data.token) {
+        localStorage.setItem("token", response.data.token);
+        message.success("Login successful!");
+      }
+    } catch (error) {
+      message.error("Login failed. Please check your credentials.");
+    } finally {
+      setLoading(false);
+    }
+  };
+
+  const onFinishFailed = (errorInfo) => {
+    console.log("Failed:", errorInfo);
+  };
+
+  return (
+    <div className="flex items-center justify-center min-h-screen bg-gray-100">
+      <div className="p-6 bg-white shadow-md rounded-md w-96">
+        <h2 className="text-2x1 font-bold text-center mb-4">Login</h2>
+        <Form
+          name="login"
+          initialValues={{ remember: true }}
+          onFinish={onFinish}
+          onFinishFailed={onFinishFailed}
+          autoComplete="off"
+        >
+          <Form.Item
+            name="username"
+            rules={[{ required: true, message: "Please input your username!" }]}
+          >
+            <Input placeholder="Username" />
+          </Form.Item>
+
+          <Form.Item
+            name="password"
+            rules={[{ required: true, message: "Please input your password!" }]}
+          >
+            <Input.Password placeholder="Password" />
+          </Form.Item>
+
+          <Form.Item className="text-center">
+            <Button
+              type="primary"
+              htmlType="submit"
+              className="w-full"
+              loading={loading}
+            >
+              Login
+            </Button>
+          </Form.Item>
+        </Form>
+      </div>
+    </div>
+  );
+};
+
+export default LoginPage;
